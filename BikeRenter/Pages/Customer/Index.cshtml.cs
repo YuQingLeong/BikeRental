af545fdb3c73ca008a827fdfe17d9c1991bf5f68
nleong <nleong@NLEONG02-PC>
2024-03-22 21:09:48 -0500

    Add project files.

---
+++ BikeRenter/Pages/Customer/Index.cshtml.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace BikeRenter.Pages.Customer
+{
+    public class IndexModel : PageModel
+    {
+        public List<CustomerInfo> listCustomers = new List<CustomerInfo>();
+        public void OnGet()
+        {
+            try
+            {
+                String connectionString = "Data Source =.\\sqlexpress; Initial Catalog = BikeRental; Integrated Security = True; Encrypt = True; Trust Server Certificate = True;";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM Customer";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read()) 
+                            {
+                                CustomerInfo customerInfo = new CustomerInfo();
+                                customerInfo.CustomerID = "" + reader.GetInt32(0);
+                                customerInfo.name = reader.GetString(1);
+                                customerInfo.email = reader.GetString(2);
+                                customerInfo.phone = reader.GetString(3);
+                                customerInfo.address = reader.GetString(4);
+
+                                listCustomers.Add(customerInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+        }
+    }
+
+    public class CustomerInfo
+    {
+        public String CustomerID;
+        public String name;
+        public String email;
+        public String phone;
+        public String address;
+    }
+
+
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿CREATE Table Customer (
+	CustomerID INT NOT NULL PRIMARY KEY IDENTITY,
+	name VARCHAR (100) NOT NULL,
+	email VARCHAR (150) NOT NULL UNIQUE,
+	phone VARCHAR (20) NULL,
+	address VARCHAR (100) NULL
+);
+
+INSERT INTO Customer (name, email, phone, address)
+VALUES
+('Natalie', 'natalie@gmail.com', '+13161468575', 'Kansas, USA'),
+('Khang', 'khang@gmail.com', '+14164577795', 'Kansas, USA'),
+('Alice', 'alice@gmail.com', '+17563638864', 'Kansas, USA'),
+('Bob', 'bob@gmail.com', '+16437784354', 'Kansas, USA');
